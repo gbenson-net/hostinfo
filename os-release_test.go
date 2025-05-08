bea3cbcf1b0043bc476c257a9d038b17a6c31316
Gary Benson <gary@gbenson.net>
2025-05-08 21:10:42 +0100

    Extend /etc/os-release tests

--- os-release_test.go
+++ os-release_test.go
@@ -1,12 +1,15 @@
 package hostinfo
 
 import (
+	_ "embed"
 	"testing"
 
+	"gbenson.net/go/invoker"
+	"gbenson.net/go/logger"
 	"gotest.tools/v3/assert"
 )
 
-func TestGatherOSRelease(t *testing.T) {
+func TestGatherOSRelease_live(t *testing.T) {
 	r := assertExec(t, gatherOSRelease)
 	assert.Assert(t, len(r.OS) > 0)
 
@@ -18,6 +21,63 @@ func TestGatherOSRelease(t *testing.T) {
 		assert.Check(t, r.OS["name"] == "Ubuntu")
 		assert.Check(t, r.OS["id_like"] == "debian")
 	default:
-		t.Errorf("unhandled: %q", r.OS["name"])
+		logger.Ctx(testctx(t)).Warn().
+			Str("name", r.OS["name"]).
+			Str("id", r.OS["id"]).
+			Msg("Unhandled OS")
 	}
+}
+
+func TestGatherOSRelease_fail(t *testing.T) {
+	want := &InvalidLineError{"os_test", "x"}
+	mock := invoker.NewMock(t)
+	mock.ExpectInvoke("cat", "/etc/os-release").Returns(nil, want)
+
+	r, err := invoke(t, mock, gatherOSRelease)
+	assert.Equal(t, err, want)
+	assert.Equal(t, len(r.OS), 0)
+}
+
+func TestGatherOSRelease_empty(t *testing.T) {
+	mock := invoker.NewMock(t)
+	mock.ExpectInvoke("cat", "/etc/os-release").Returns([]byte{}, nil)
+
+	r := assertMock(t, gatherOSRelease, mock)
+	assert.Equal(t, len(r.OS), 0)
+}
+
+//go:embed resources/os-release.fedora
+var fedoraOSRelease []byte
+
+func TestGatherOSRelease_fedora(t *testing.T) {
+	mock := invoker.NewMock(t)
+	mock.ExpectInvoke("cat", "/etc/os-release").Returns(fedoraOSRelease, nil)
+
+	r := assertMock(t, gatherOSRelease, mock)
+	assert.Equal(t, len(r.OS), 23)
+
+	assert.Equal(t, r.OS["name"], "Fedora Linux")
+	assert.Equal(t, r.OS["id"], "fedora")
+	assert.Equal(t, r.OS["version"], "42 (Container Image Prerelease)")
+	assert.Equal(t, r.OS["version_id"], "42")
+
+	assertNotHasKey(t, r.OS, "id_like")
+}
+
+//go:embed resources/os-release.ubuntu
+var ubuntuOSRelease []byte
+
+func TestGatherOSRelease_ubuntu(t *testing.T) {
+	mock := invoker.NewMock(t)
+	mock.ExpectInvoke("cat", "/etc/os-release").Returns(ubuntuOSRelease, nil)
+
+	r := assertMock(t, gatherOSRelease, mock)
+	assert.Equal(t, len(r.OS), 12)
+
+	assert.Equal(t, r.OS["name"], "Ubuntu")
+	assert.Equal(t, r.OS["id"], "ubuntu")
+	assert.Equal(t, r.OS["id_like"], "debian")
+	assert.Equal(t, r.OS["version"], "22.04.5 LTS (Jammy Jellyfish)")
+	assert.Equal(t, r.OS["version_id"], "22.04")
+	assert.Equal(t, r.OS["pretty_name"], "Ubuntu 22.04.5 LTS")
 }
